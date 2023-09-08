4519de2aa88641b20a8a53e6f8b718b9e125049e
Washington Rodrigues <washingtonjunio16@gmail.com>
2023-09-08 13:04:50 -0300

    feat: fake routes

--- dashboard/src/router/index.js
+++ dashboard/src/router/index.js
@@ -1,8 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+const Home = () => import('../views/Home/index.vue')
+const Feedbacks = () => import('../views/Feedbacks/index.vue')
+const Credentials = () => import('../views/Credentials/index.vue')
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/feedbacks',
+      name: 'Feedbacks',
+      component: Feedbacks,
+      meta: {
+        hasAuth: true
+      }
+    },
+    {
+      path: '/credentials',
+      name: 'Credentials',
+      component: Credentials
+    },
+    {
+      path: '/:pathMatch(.*)',
+      redirect: {
+        name: 'Home'
+      }
+    }
   ]
 })
 
---
+++ dashboard/src/views/Home/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="text-brand-main bg-black">Home here</h1>
+</template>
