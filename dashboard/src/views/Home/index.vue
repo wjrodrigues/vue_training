d76408b6311beba1150180e08199046eb5e57eb3
Washington Rodrigues <washingtonjunio16@gmail.com>
2023-09-23 18:17:02 -0300

    feat: create minimal index home

--- dashboard/src/views/Home/index.vue
+++ dashboard/src/views/Home/index.vue
@@ -1,3 +1,33 @@
 <template>
-  <h1 class="text-brand-main bg-black">Home here</h1>
+  <custom-header
+    @create-account="HandleAccountCreate"
+    @login="handleLogin"
+  />
+  <contact />
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">feedback © 2023</p>
+  </div>
 </template>
+
+<script>
+import CustomHeader from './CustomHeader.vue';
+import Contact from './Contact.vue';
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+
+export default {
+  components: {CustomHeader , Contact},
+  setup() {
+    const router = useRouter()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (token) {
+        router.push({ name: 'Feedbacks'})
+      }
+    }),
+    function handleLogin() {},
+    function HandleAccountCreate() {}
+  }
+}
+</script>
