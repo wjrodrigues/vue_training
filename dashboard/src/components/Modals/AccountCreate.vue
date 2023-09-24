3dbaea5a6d6e8176cd9f5e0af3d2cbc8c26ec334
Washington Rodrigues <washingtonjunio16@gmail.com>
2023-09-24 19:13:18 -0300

    feat: create modals AccountCreate and Login struct

---
+++ dashboard/src/components/Modals/AccountCreate.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Account create</h1>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ dashboard/src/components/Modals/Login.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Modal login</h1>
+</template>
+
+<script>
+export default {
+  name: 'modal-login'
+}
+</script>
