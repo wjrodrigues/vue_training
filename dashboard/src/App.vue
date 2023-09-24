e77e42e97b3257a3e67a1944d1f24f363deeb5a9
Washington Rodrigues <washingtonjunio16@gmail.com>
2023-09-24 19:14:33 -0300

    feat: add modals factory on App.vue

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -1,4 +1,12 @@
 <template>
+  <modal-factory />
   <RouterView />
 </template>
 
+<script>
+import ModalFactory from './components/Modals/Factory.vue'
+
+export default {
+  components: { ModalFactory }
+}
+</script>
