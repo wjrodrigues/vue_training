e1aedd1a20fee55c28aa327fd7f5cf6e0c7a06d5
Washington Rodrigues <washingtonjunio16@gmail.com>
2023-10-08 17:31:54 -0300

    feat: configure vue-toastification

--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -4,8 +4,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Toast
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
 const app = createApp(App)
 
 app.use(router)
+app.use(Toast)
 
 app.mount('#app')
